7105b221ac395ec7cc7a32f40c444671f293429e
N-V-Hung <nongvanhungcb2k5@gmail.com>
2025-09-12 13:22:39 +0700

    ttcd-prj2

---
+++ FastFood/Controllers/GioHangsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FastFood.Models;
+
+namespace FastFood.Controllers
+{
+    public class GioHangsController : Controller
+    {
+        private readonly QlbanDoAnContext _context;
+
+        public GioHangsController(QlbanDoAnContext context)
+        {
+            _context = context;
+        }
+
+        // GET: GioHangs
+        public async Task<IActionResult> Index()
+        {
+            var qlbanDoAnContext = _context.GioHangs.Include(g => g.MaKhNavigation);
+            return View(await qlbanDoAnContext.ToListAsync());
+        }
+
+        // GET: GioHangs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gioHang = await _context.GioHangs
+                .Include(g => g.MaKhNavigation)
+                .FirstOrDefaultAsync(m => m.MaGh == id);
+            if (gioHang == null)
+            {
+                return NotFound();
+            }
+
+            return View(gioHang);
+        }
+
+        // GET: GioHangs/Create
+        public IActionResult Create()
+        {
+            ViewData["MaKh"] = new SelectList(_context.KhachHangs, "MaKh", "MaKh");
+            return View();
+        }
+
+        // POST: GioHangs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MaGh,MaKh")] GioHang gioHang)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(gioHang);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MaKh"] = new SelectList(_context.KhachHangs, "MaKh", "MaKh", gioHang.MaKh);
+            return View(gioHang);
+        }
+
+        // GET: GioHangs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gioHang = await _context.GioHangs.FindAsync(id);
+            if (gioHang == null)
+            {
+                return NotFound();
+            }
+            ViewData["MaKh"] = new SelectList(_context.KhachHangs, "MaKh", "MaKh", gioHang.MaKh);
+            return View(gioHang);
+        }
+
+        // POST: GioHangs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MaGh,MaKh")] GioHang gioHang)
+        {
+            if (id != gioHang.MaGh)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(gioHang);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GioHangExists(gioHang.MaGh))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MaKh"] = new SelectList(_context.KhachHangs, "MaKh", "MaKh", gioHang.MaKh);
+            return View(gioHang);
+        }
+
+        // GET: GioHangs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gioHang = await _context.GioHangs
+                .Include(g => g.MaKhNavigation)
+                .FirstOrDefaultAsync(m => m.MaGh == id);
+            if (gioHang == null)
+            {
+                return NotFound();
+            }
+
+            return View(gioHang);
+        }
+
+        // POST: GioHangs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var gioHang = await _context.GioHangs.FindAsync(id);
+            if (gioHang != null)
+            {
+                _context.GioHangs.Remove(gioHang);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GioHangExists(int id)
+        {
+            return _context.GioHangs.Any(e => e.MaGh == id);
+        }
+    }
+}
