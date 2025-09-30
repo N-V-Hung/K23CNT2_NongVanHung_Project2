2bfcb1801ef3a34af6940e504e684ee0e10c1216
N-V-Hung <nongvanhungcb2k5@gmail.com>
2025-09-30 09:59:35 +0700

    ttcd-project2

--- FastFood/obj/Debug/net8.0/FastFood.AssemblyInfo.cs
+++ FastFood/obj/Debug/net8.0/FastFood.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FastFood")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08771700aae352432e68aecbd7f9b4947df53de1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f5cc76f00d0979653924e20e08816b20e220df7")]
 [assembly: System.Reflection.AssemblyProductAttribute("FastFood")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FastFood")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
